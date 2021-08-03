0ac3b5257a6892e2dca8b4a4b71d95684668d113
Faseem Shanavas <faseem619@gmail.com>
2021-08-03 22:28:12 +0530

    added new fonts

--- src/Components/TasksContext.js
+++ src/Components/TasksContext.js
@@ -6,13 +6,11 @@ const TasksContextProvider = ({ children }) => {
   const [tasks, setTasks] = useState([]);
   useEffect(() => {
     getCurrentTasks();
-    // setTasks(currenTasks);
   }, []);
 
   const getCurrentTasks = async () => {
     const response = await fetch("http://127.0.0.1:5000/", { mode: "cors" });
     const data = await response.json();
-    console.log(data);
     setTasks(data);
   };
 
