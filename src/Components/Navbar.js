dc023346c2f974870fab150d00692256bd9a0dea
Faseem Shanavas <faseem619@gmail.com>
2021-08-05 20:18:17 +0530

    added navbar for mobile device

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./CSS/navbar.css";
 import { RiLogoutBoxRLine } from "react-icons/ri";
 import { FcAbout } from "react-icons/fc";
 import { FcTodoList } from "react-icons/fc";
+import { FcMenu } from "react-icons/fc";
 function Navbar() {
+  const navbar = useRef(null);
+  const handleClick = () => {
+    navbar.current.classList.toggle("show");
+  };
   return (
-    <nav>
-      <h1>
-        <FcTodoList className="nav-icons" />
-        MY TODO APP
-      </h1>
-      <h2>
-        Log Out <RiLogoutBoxRLine className="nav-icons" />
-      </h2>
-      <h2>
-        About <FcAbout className="nav-icons" />
-      </h2>
-    </nav>
+    <>
+      <nav>
+        <h1>
+          <FcTodoList className="nav-icons" />
+          MY TODO APP
+        </h1>
+        <h2>
+          Log Out <RiLogoutBoxRLine className="nav-icons" />
+        </h2>
+        <h2>
+          About <FcAbout className="nav-icons " />
+        </h2>
+        <FcMenu className="nav-icons menu-btn" onClick={handleClick} />
+      </nav>
+      <div className="nav-toggle" ref={navbar}>
+        <h2 className="nav-list-item">Log Out</h2>
+        <h2 className="nav-list-item">About</h2>
+      </div>
+    </>
   );
 }
 
